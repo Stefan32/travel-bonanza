2276ddaae3326d8ec870f6125203b84985431222
Stefan <stefannel16@gmail.com>
2017-03-04 11:27:38 +0200

    worked on link highlighting when scrolled to

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,11 +1,20 @@
 import $ from 'jquery'; /* Because you use jquery in the constructor function */
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints'; /* This is to make use of Waypoints */
+import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
   constructor() {
     this.siteHeader = $(".site-header"); /* This selects elements with site-header as a class */
     this.headerTriggerElement = $(".large-hero__title"); /* This selects the trigger element, when element reaches the top of the screen it will trigger the event */
     this.createHeaderWaypoint(); /* We want the waypoint to be created as soon as the page loads, so we need to call and run the method from the constructor */
+    this.pageSections = $(".page-section"); /* this selects all elements with a class of page-section */
+    this.headerLinks = $(".primary-nav a"); /* Look inside the primary nav element and grab all link elements */
+    this.createPageSectionWaypoints();
+    this.addSmoothScrolling();
+  }
+
+  addSmoothScrolling() {
+    this.headerLinks.smoothScroll();
   }
 
   createHeaderWaypoint() {
@@ -19,6 +28,36 @@ class StickyHeader {
           that.siteHeader.removeClass("site-header--dark"); /* Targets the element and gives class */
         }
       } /* What you want to happen when triggered, add a modifier class to the header element */
+    });
+  }
+
+  createPageSectionWaypoints() { /* New method that will create waypoints for each element selected */
+    var that = this;
+    this.pageSections.each(function() {
+      var currentPageSection = this;
+      new Waypoint({ /* Create waypoint for each element when scrolling down */
+        element: currentPageSection,
+        handler: function(direction) {
+          if (direction == "down") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            that.headerLinks.removeClass("is-current-link");
+            $(matchingHeaderLink).addClass("is-current-link");
+          }
+        },
+        offset: "18%" /* makes the event trigger 18% sooner when scrolling down */
+      });
+
+      new Waypoint({ /* Create waypoint for each element when scrolling up */
+        element: currentPageSection,
+        handler: function(direction) {
+          if (direction == "up") {
+            var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+            that.headerLinks.removeClass("is-current-link");
+            $(matchingHeaderLink).addClass("is-current-link");
+          }
+        },
+        offset: "-40%" /* makes the event trigger 40% sooner when scrolling up */
+      });
     });
   }
 }
